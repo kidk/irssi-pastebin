996875c9699b4f36c5030816d938bddb00b65741
Samuel Vandamme <samuel@sava.be>
2010-01-19 14:01:13 +0100

    1.0 Release

--- pastebin.pl
+++ pastebin.pl
@@ -1,15 +1,38 @@
 use strict;
 #
 # Irssi Pastebin
-# Copyright (C) Samuel Vandamme
 #
-
+# Copyright (C) 2010 Samuel Vandamme
 #
-# Check README for license
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Change Log:
+# v1.0b:
+#        - Initial release
+# v1.0:
+#        - Looks stable
+#
+# Requirements:
+#        - perl-libwww
+#
+# Problems ?
+#        http://github.com/kidk/irssi-pastebin/issues
+#        OR
+#        email : samuel@sava.be
 
 use vars qw($VERSION %IRSSI);
-$VERSION = "1.0b";
+$VERSION = "1.0";
 %IRSSI = (
 	authors 	=>	"Samuel 'kidk' Vandamme",
 	contact		=> 	"samuel\@sava.be",
